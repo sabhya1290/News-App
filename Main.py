411d65b8fac780fe93fe9788579169ee94a0ba6c
sabhya1290 <sabhya1290@gmail.com>
2025-06-08 15:37:55 +0530

    almost_done

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ class News_app(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("News Buddy")
-        self.setGeometry(300,200,500,500) 
+        self.setGeometry(400,200,700,600) 
         self.stack = QStackedLayout()
         self.base()
         self.setLayout(self.stack)
@@ -20,16 +20,16 @@ class News_app(QWidget):
 
         self.header= QLabel("News Buddy",self)
         self.news_label=QLabel("Want to see Top Headlines or Everything:",self)
-        headline=QPushButton("Top Headlines",self)
-        everything=QPushButton("Everything",self)
+        self.headline=QPushButton("Top Headlines",self)
+        self.everything=QPushButton("Everything",self)
         
         l1=QVBoxLayout()
         l1.addWidget(self.header)
         l1.addWidget(self.news_label)
 
         h1=QHBoxLayout()
-        h1.addWidget(headline)
-        h1.addWidget(everything)
+        h1.addWidget(self.headline)
+        h1.addWidget(self.everything)
         l1.addLayout(h1)
         l1.setAlignment(Qt.AlignTop)
         
@@ -54,8 +54,8 @@ class News_app(QWidget):
     
         self.layout1.setLayout(l1)
         self.stack.addWidget(self.layout1)
-        headline.clicked.connect(self.Headlines)
-        everything.clicked.connect(self.Everything)
+        self.headline.clicked.connect(self.Headlines)
+        self.everything.clicked.connect(self.Everything)
 
     def initUI(self):
         pass
@@ -109,8 +109,12 @@ class News_app(QWidget):
         self.news_url= QLabel(self)
         previous_page=QPushButton("Go Back To Previous Page",self)
         self.image_label=QLabel(self)
-        self.image_label.setFixedSize(500, 500) 
+        self.image_label.setFixedSize(400, 400) 
         self.image_label.setAlignment(Qt.AlignCenter)
+        # self.error_label=QLabel(self)
+        self.next_article=QPushButton("Next Article",self)
+        self.previous_article=QPushButton("Previous Article",self)
+        self.page=0
 
         
         self.l2=QVBoxLayout()  
@@ -121,21 +125,23 @@ class News_app(QWidget):
         self.hbox.addWidget(self.button)
         self.l2.addLayout(self.hbox)
         
-        self.button.clicked.connect(self.search_headlines)
-        """
-        l2.addLayout(hbox)
-        l2.addWidget(self.news_title)
+        
+        self.l2.addLayout(self.hbox)
+        self.l2.addWidget(self.news_title)
+        # self.l2.addWidget(self.error_label)
         h=QHBoxLayout()
         v=QVBoxLayout()
         v.addWidget(self.news_description)
         v.addWidget(self.news_url)
         h.addLayout(v)
         h.addWidget(self.image_label)
-        l2.addLayout(h)
-        """
-        self.l2.addWidget(previous_page)
+        self.l2.addLayout(h)
+        self.h1=QHBoxLayout()
+        self.h1.addWidget(previous_page)
+        self.l2.addLayout(self.h1)
         self.layout2.setLayout(self.l2)
         self.stack.addWidget(scroll)
+        self.button.clicked.connect(self.search_headlines)
 
         
         self.l2.setAlignment(Qt.AlignTop)
@@ -145,6 +151,7 @@ class News_app(QWidget):
         self.news_title.setObjectName("news_title")
         self.news_description.setObjectName("description")
         self.news_url.setObjectName("url")
+        # self.error_label.setObjectName("error")
 
 
         self.layout2.setStyleSheet("""
@@ -175,6 +182,7 @@ class News_app(QWidget):
                             font-size:20px;
                             font-family:Arial;       
                             }
+                            
                             """)
         self.stack.setCurrentIndex(1)
         self.stack.setCurrentWidget(scroll)
@@ -186,6 +194,10 @@ class News_app(QWidget):
         
         
         self.layout2 = QWidget()
+        scroll = QScrollArea()
+        scroll.setWidgetResizable(True)
+        scroll.setWidget(self.layout2)
+
 
         self.header= QLabel("News Buddy",self)
         self.news_Topic=QLabel("Enter Topic:",self)
@@ -198,29 +210,35 @@ class News_app(QWidget):
         self.image_label=QLabel(self)
         self.image_label.setFixedSize(300, 300)  
         self.image_label.setAlignment(Qt.AlignCenter)
+        self.next_article=QPushButton("Next Article",self)
+        self.previous_article=QPushButton("Previous Article",self)
+        self.page=0
 
         
-        l2=QVBoxLayout()  
-        l2.addWidget(self.header)
+        self.l2=QVBoxLayout()  
+        self.l2.addWidget(self.header)
         hbox=QHBoxLayout()
         hbox.addWidget(self.news_Topic)
         hbox.addWidget(self.topic_input)
         hbox.addWidget(self.button_n)
-        l2.addLayout(hbox)
-        l2.addWidget(self.news_title)
+        self.l2.addLayout(hbox)
+        self.l2.addWidget(self.news_title)
         h=QHBoxLayout()
         v=QVBoxLayout()
         v.addWidget(self.news_description)
         v.addWidget(self.news_url)
         h.addLayout(v)
         h.addWidget(self.image_label)
-        l2.addLayout(h)
-        l2.addWidget(previous_page)
+        self.l2.addLayout(h)
+        self.h1=QHBoxLayout()
+        self.h1.addWidget(previous_page)
+        self.l2.addLayout(self.h1)
 
-        self.layout2.setLayout(l2)
-        self.stack.addWidget(self.layout2)
+        self.layout2.setLayout(self.l2)
+        self.stack.addWidget(scroll)
+        self.button_n.clicked.connect(self.search_everything)
 
-        l2.setAlignment(Qt.AlignTop)
+        self.l2.setAlignment(Qt.AlignTop)
 
         self.header.setObjectName("Title")
         self.news_Topic.setObjectName("Label")
@@ -250,7 +268,8 @@ class News_app(QWidget):
                             }
                             QLabel#description{
                             font-size:20px;
-                            font-family:Arial;       
+                            font-family:Arial;
+                            color:black;       
                             }
                             QLabel#url{
                             font-size:20px;
@@ -258,7 +277,8 @@ class News_app(QWidget):
                             }
                             """)
         self.stack.setCurrentIndex(1)
-        self.button_n.clicked.connect(self.search_everything)
+        self.stack.setCurrentWidget(scroll)
+
         previous_page.clicked.connect(self.previous_page_func)
 
     def search_headlines(self):
@@ -271,10 +291,45 @@ class News_app(QWidget):
             data=response.json()
             totalresult=data['totalResults']
             
-            self.display_news(data)
-            self.layout2.deleteLater()
-            self.stack.setCurrentIndex(1)
-                
+            if self.page==0:
+                self.h1.addWidget(self.next_article)
+                self.h1.addWidget(self.previous_article)
+                self.layout2.setLayout(self.h1)
+            self.next_article.clicked.connect(self.next_page_func)
+            self.previous_article.clicked.connect(self.previous_article_func)
+
+            news=data['articles']
+
+            if self.page>totalresult:
+                self.next_article.setEnabled(False)
+            else:
+                self.next_article.setEnabled(True)
+
+            if self.page<1:
+                self.previous_article.setEnabled(False)
+            else:
+                self.previous_article.setEnabled(True)
+
+            self.news_title.setText(news[self.page]['title'])
+            print(self.page)
+            self.news_title.setWordWrap(True)
+            self.news_description.setText(news[self.page]['description'])
+            self.news_description.setWordWrap(True)
+            self.news_description.setStyleSheet("font-size:20px;"
+                                            "color: black")
+            self.news_url.setText(f"url={news[self.page]['url']}")
+            self.news_url.setWordWrap(True)
+            url_image=news[self.page]['urlToImage']
+            r=requests.get(url_image)
+            if r.status_code==200:
+                image_data = BytesIO(r.content)
+                pixmap = QPixmap()
+                pixmap.loadFromData(image_data.read())
+                self.image_label.setPixmap(pixmap.scaled(
+                            self.image_label.width(), self.image_label.height(), Qt.KeepAspectRatio))
+            else:
+                pass
+            
         except requests.exceptions.HTTPError as http_error: 
             match response.status_code: 
                 case 400: 
@@ -301,8 +356,6 @@ class News_app(QWidget):
         
         except IndexError:
             self.display_error("No news found for the given topic")
-        # except KeyError:
-        #     self.display_error("Please first search something")
         
     def search_everything(self):
         try:
@@ -313,22 +366,31 @@ class News_app(QWidget):
             response=requests.get(url)
             response.raise_for_status()
             data=response.json()
+
+            self.h1.addWidget(self.next_article)
+            self.h1.addWidget(self.previous_article)
+            self.layout2.setLayout(self.h1)
+            self.next_article.clicked.connect(self.next_page_func)
+            self.previous_article.clicked.connect(self.previous_article_func)
             
-            self.news_title.setText(data['articles'][1]['title'])
+            self.news_title.setText(data['articles'][self.page]['title'])
             self.news_title.setWordWrap(True)
-            self.news_description.setText(data['articles'][1]['description'])
+            self.news_description.setText(data['articles'][self.page]['description'])
             self.news_description.setWordWrap(True)
-            self.news_url.setText(f"url={data['articles'][1]['url']}")
+            self.news_description.setStyleSheet("font-size:20px;"
+                                            "color: black")
+            self.news_url.setText(f"url={data['articles'][self.page]['url']}")
             self.news_url.setWordWrap(True)
-            url_image=data['articles'][1]['urlToImage']
+            url_image=data['articles'][self.page]['urlToImage']
             r=requests.get(url_image)
-            image_data = BytesIO(r.content)
-            pixmap = QPixmap()
-            pixmap.loadFromData(image_data.read())
-            # self.image_label.setPixmap(pixmap)
-            self.image_label.setPixmap(pixmap.scaled(
-                        self.image_label.width(), self.image_label.height(), Qt.KeepAspectRatio))
-            
+            if r.status_code==200:
+                image_data = BytesIO(r.content)
+                pixmap = QPixmap()
+                pixmap.loadFromData(image_data.read())
+                self.image_label.setPixmap(pixmap.scaled(
+                            self.image_label.width(), self.image_label.height(), Qt.KeepAspectRatio))
+            else:
+                pass
             
         except requests.exceptions.HTTPError as http_error: 
             match response.status_code: 
@@ -361,12 +423,15 @@ class News_app(QWidget):
                
     def display_error(self,message):
         self.news_title.setText("")
-        self.news_description.setText(message)
         self.news_url.setText("")
+        self.news_description.setText(message)
         self.image_label.setPixmap(QPixmap())
+        self.news_description.setStyleSheet("font-size:30px;"
+                                            "color: red")
+
         
     def display_news(self,data):
-
+        pass
         for i in range(5):
                 self.news_title=QLabel(self)
                 self.news_description=QLabel(self)
@@ -383,17 +448,17 @@ class News_app(QWidget):
                 self.news_url.setWordWrap(True)
                 url_image=data['articles'][i]['urlToImage']
                 
-                try:
-                        r=requests.get(url_image)
-                        image_data = BytesIO(r.content)
-                        pixmap = QPixmap()
-                        pixmap.loadFromData(image_data.read())
-                        self.image_label.setPixmap(pixmap.scaled(
-                                        self.image_label.width(),
-                                        self.image_label.height(),
-                                            Qt.KeepAspectRatio))
-                except:
-                        continue
+                
+                r=requests.get(url_image)
+                image_data = BytesIO(r.content)
+                pixmap = QPixmap()
+                pixmap.loadFromData(image_data.read())
+                self.image_label.setPixmap(pixmap.scaled(
+                                self.image_label.width(),
+                                self.image_label.height(),
+                                    Qt.KeepAspectRatio))
+                
+                        
                 
                 
                 self.l2.addWidget(self.news_title)
@@ -440,8 +505,6 @@ class News_app(QWidget):
                             """)
                 # self.button.clicked.connect(self.search_headlines)
 
-        
-
 
     def previous_page_func(self):
         current_widget = self.stack.currentWidget()
@@ -449,6 +512,19 @@ class News_app(QWidget):
         self.layout2.deleteLater()
         self.stack.setCurrentIndex(0)
 
+    def next_page_func(self):
+        self.page+=1
+        if self.headline.clicked:
+            self.search_headlines()
+        else:
+            self.search_everything()
+
+    def previous_article_func(self):
+        self.page-=1
+        if self.headline.clicked:
+            self.search_headlines()
+        else:
+            self.search_everything()
 
 def main():
     app = QApplication(sys.argv)
